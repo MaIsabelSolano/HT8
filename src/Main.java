f8e59299bd56da86290a77ec84b1a06ef4b6f2aa
MaIsabelSolano <misolanob@gmail.com>
2021-04-22 23:16:10 -0600

    edicion en clase main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.*;
+
 /**
  * Universidad del Valle de Gautemala
  * Facultad de Ingeniería
@@ -9,7 +12,37 @@
  */
 
 public class Main {
-    public static void Main (String[] args){
+    public static void main (String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int opcion_usuario = 0;
+
+        //Obtener la priority queue de pacientes
+
+
+        //Iniciar el programa
+        while (opcion_usuario != 2){
+            try {
+                System.out.println("______________________________________");
+                System.out.println("Qué desea realizar a continuación?\n");
+                System.out.println("1) Obtener al siguiente paciente");
+                System.out.println("2) Salir del programa\n");
+
+                opcion_usuario = scan.nextInt();
+
+                if (opcion_usuario == 1 ){
+                    //obtener al paciente que urge más
+                }
+                else {
+                    System.out.println("\n[Por favor ingrese una opcion valida]");
+                }
+
+            }
+            catch (InputMismatchException ime){
+                System.out.println("\n[Por favor ingrese una opcion valida]");
+                scan.nextLine();
+            }
+        }
 
     }
 }
